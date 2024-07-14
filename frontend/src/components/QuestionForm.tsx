7941aee3a26936012b8bc5ef591e8e4897e7893f
Aniket <annudubey1026@gmail.com>
2024-07-14 18:01:14 +0530

    feat : added custom questions for user

--- frontend/src/components/QuestionForm.tsx
+++ frontend/src/components/QuestionForm.tsx
@@ -4,19 +4,57 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useStreamingResponse } from "@/hooks/useStreaming";
 import ReactMarkdown from 'react-markdown';
+import QuestionCard from "./QuestionCard";
+
+const dummyQuestions = [
+  {
+    question: "What are the fundamental rights in the Indian Constitution?",
+    description: "Learn about the basic rights guaranteed to all citizens of India."
+  },
+  {
+    question: "The Judicial System of India",
+    description: "What are the three levels of the Indian judiciary"
+  },
+  {
+    question: "What is the structure of the Indian judiciary?",
+    description: "Explore the organization of courts in India's legal system."
+  },
+];
+
 
 export default function QuestionForm() {
   const [query, setQuery] = useState("");
+  const [showCustomQuestion, setShowCustomQuestion] = useState(true);
   const { response, isLoading, error, streamResponse } = useStreamingResponse()
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setQuery("");
     await streamResponse(query);
-    console.log('Executed');
   };
+
+  const handleCardClick = async (question: string) => {
+    setShowCustomQuestion(false);
+    setQuery(question);
+    await streamResponse(question);
+    setQuery("");
+  }
 
   return (
     <div className="flex flex-col w-full">
+      {showCustomQuestion && (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+          {dummyQuestions.map((q, index) => (
+            <QuestionCard
+              key={index}
+              question={q.question}
+              description={q.description}
+              onAsk={handleCardClick}
+            />
+          ))}
+        </div>
+      )}
+
       {isLoading && <p className="mt-2 text-gray-600">Loading...</p>}
       {error && <p className="mt-2 text-red-600">Error: {error}</p>}
       {response && (
@@ -24,15 +62,15 @@ export default function QuestionForm() {
           <ReactMarkdown>{response}</ReactMarkdown>
         </div>
       )}
-      <form onSubmit={handleSubmit} className="flex w-full mt-4">
+      <form onSubmit={handleSubmit} className="flex w-full mt-4 p-6">
         <input
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Ask a question about the Indian Constitution"
-          className="p-2 outline-none flex flex-grow bg-gray-100 rounded-l-lg"
+          className="p-2 outline-none flex flex-grow bg-gray-200 rounded-lg"
         />
-        <Button type="submit" className="rounded-r-lg">
+        <Button type="submit" className="rounded-r-lg mx-4">
           Ask
         </Button>
       </form>
