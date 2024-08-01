98438e8948d6a7504cefb7e8190befc26d72567d
Aniket <annudubey1026@gmail.com>
2024-08-01 14:48:13 +0530

    feat : created user and query model

---
+++ backend/app/models/query.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime , ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from app.db.base_class import Base
+
+
+class Query(Base):
+    __tablename__ = 'queries'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    query_text = Column(String)
+    answer_text = Column(String)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    user = relationship("User", back_populates="queries")
---
+++ backend/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+from app.db.base_class import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
