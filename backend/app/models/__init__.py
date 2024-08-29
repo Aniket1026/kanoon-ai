024d8b389c307b11744b5f95209519cff1c55516
Aniket <annudubey1026@gmail.com>
2024-08-29 19:28:45 +0530

    chore : user model updated

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,2 @@
 from .user import User
 from .query import Query
-from .base_class import Base
-
-__all__ = ["User", "Query","Base"]
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, Integer, String, DateTime, func
 from .base_class import Base
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    queries = relationship("Query", back_populates="user")
