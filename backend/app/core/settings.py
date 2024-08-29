e6ad145dcaf5a7fee8d20012c186b09aa13d3847
Aniket <annudubey1026@gmail.com>
2024-08-29 19:59:40 +0530

    chore: Add POSTGRES_URL variable for database configuration

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -7,5 +7,6 @@ class Settings :
     PDF_PATH = os.getenv("PDF_PATH")
     CHROMA_DB_PATH = os.getenv("CHROMA_DB_PATH")
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+    POSTGRES_URL = os.getenv("POSTGRES_URL")
 
 settings = Settings()    
