f20447582a3c03330eaafb20b7fce8bb9880b80f
Aniket <annudubey1026@gmail.com>
2024-09-01 18:59:44 +0530

    chore : moved home page to chat folder

---
+++ frontend/src/app/chat/layout.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react';
+import Header from '../../components/Header';
+import '../globals.css';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+});
+
+export default function ChatLayout({ children }: { children: ReactNode }) {
+    return (
+        <div className={`flex flex-col justify-center items-center h-screen ${roboto.className}`}>
+            <Header />
+            <main className="w-full flex flex-grow">
+                {children}
+            </main>
+        </div>
+    );
+}
---
+++ frontend/src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import QuestionForm from '../../components/QuestionForm';
+
+export default function HomePage() {
+    return (
+        <div className="p-4 flex flex-col items-center justify-between  min-w-full">
+            <h1 className="text-2xl mb-4 font-light text-slate-400">Welcome to Kanoon AI</h1>
+            <QuestionForm />
+        </div>
+    );
+}
+
